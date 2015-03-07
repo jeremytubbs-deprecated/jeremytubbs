e4d49859a2a6d01695b12909584683deeaaedac1
Jeremy Tubbs <jeremytubbs@gmail.com>
2015-03-07 16:23:06 +0000

    switching frontend to uikit

--- gulpfile.js
+++ gulpfile.js
@@ -11,30 +11,19 @@ var elixir = require('laravel-elixir');
  |
  */
 
-var paths = {
-	'bootstrap': './vendor/bower_components/bootstrap-sass-official/assets/',
-	'jquery': './vendor/bower_components/jquery/',
-	'codemirror': './vendor/bower_components/codemirror/',
-	'commonmark': './vendor/bower_components/commonmark/',
-	'angular': './vendor/bower_components/angular/',
-	'ngCodemirror': './vendor/bower_components/angular-ui-codemirror/',
-    'ngSelect': './vendor/bower_components/angular-selectize.js/'
-}
-
 // only move files to public for now; minify later
 elixir(function(mix) {
-    mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts')
-    .copy(paths.bootstrap + 'stylesheets/bootstrap/**', 'resources/assets/sass/bootstrap')
-    .copy(paths.bootstrap + 'javascripts/bootstrap.js', 'public/js/vendor/bootstrap.js')
-    .copy(paths.jquery + 'dist/jquery.js', 'public/js/vendor/jquery.js')
-    .copy(paths.codemirror + 'lib/codemirror.js', 'public/js/posts/codemirror.js')
-    .copy(paths.codemirror + 'lib/codemirror.css', 'public/css/posts/codemirror.css')
-    .copy(paths.commonmark + 'dist/commonmark.js', 'public/js/posts/commonmark.js')
-    .copy(paths.angular + 'angular.js', 'public/js/angular/angular.js')
-    .copy(paths.ngCodemirror + 'ui-codemirror.js', 'public/js/posts/ui-codemirror.js')
-    .copy(paths.ngSelect + 'angular-selectize.js', 'public/js/posts/angular-selectize.js')
-    .copy('./resources/assets/js/postsApp.js', 'public/js/apps/postsApp.js')
-    .copy('./resources/assets/js/jasny-file-input.js', 'public/js/posts/jasny-file-input.js')
-    .sass('main.scss');
+    mix.sass('main.scss');
+    mix.sass('admin.scss');
+    mix.scripts(['vendor/jquery.js', 'vendor/uikit/uikit.js'], 'public/js/vendor.js');
+    mix.scripts([
+        "admin/codemirror/codemirror.js",
+        "admin/codemirror/mode/markdown.js",
+        "admin/codemirror/mode/xml.js",
+        "admin/codemirror/mode/gfm.js",
+        "admin/codemirror/addon/overlay.js",
+        "admin/marked.js",
+        "vendor/uikit/components/htmleditor.js"
+    ], 'public/js/admin.js');
 });
 
